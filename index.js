051a8a97294ab582484f8c23435d500a8112d02a
unknown <brianpatterson2013@gmail.com>
2022-03-06 15:18:49 -0800

    Add mailer and overwrite file

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const puppeteer = require('puppeteer')
+const nodemailer = require('nodemailer');
 const fs = require('fs')
 const buffer = new Buffer.alloc(1024)
 
-async function compareTimestamps(newTimestamp) {
-    let oldTimestamp =  fs.readFileSync('./seconduseTimestamp.txt', {encoding:'utf8', flag:'r'})
-    console.log("old" + oldTimestamp)
-    console.log("new" + newTimestamp)
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: 'secondusetimestampmailer@gmail.com',
+      pass: 'secondusetimestamp',
+    },
+});
+
+const buildEmailObject = (newTimestamp) => { 
+    return {
+        from: '"Second Use Mailer" <secondusetimestampmailer@gmail.com>', // sender address
+        to: "torilovesbrian@gmail.com", // list of receivers
+        subject: "New Stuff!", // Subject line
+        text: `Second Use ${newTimestamp}`, // plain text body
+        html: `<b>Second Use ${newTimestamp}</b>`, // html body
+    }
 }
 
+async function compareTimestamps(newTimestamp) {
+    let oldTimestamp =  fs.readFileSync('./seconduseTimestamp.txt', {encoding:'utf8', flag:'r'})
+
+    if (oldTimestamp !== newTimestamp) {
+        transporter.sendMail(buildEmailObject(newTimestamp)).then(info => {
+            console.log({info});
+        }).catch(console.error)
+        fs.writeFileSync('./seconduseTimestamp.txt', newTimestamp)
+        console.log("Updated timestamp")
+    } else {
+        console.log("No new updates")
+    }
+}
+ 
 async function scrape() {
     const browser = await puppeteer.launch({})
     const page = await browser.newPage()
